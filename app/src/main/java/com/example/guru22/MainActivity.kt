be597ddaa3b04f784933ae3f9b117e3c3ab5c35a
goneii <102407020+goneii@users.noreply.github.com>
2023-07-28 13:28:44 +0900

    main

---
+++ app/src/main/java/com/example/guru22/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.guru22
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    lateinit var coinButton: ImageButton
+    lateinit var diceButton: ImageButton
+    lateinit var ImageView : ImageView
+    lateinit var ImageView2 : ImageView
+    lateinit var ImageView4 : ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        coinButton=findViewById<ImageButton>(R.id.coinButton)
+        diceButton=findViewById<ImageButton>(R.id.diceButton)
+
+        ImageView=findViewById(R.id.imageView)
+        ImageView2=findViewById(R.id.imageView2)
+        ImageView4=findViewById(R.id.imageView4)
+
+
+
+        coinButton.setOnClickListener{
+            var intent = Intent(this,coinButtonresult::class.java)
+            startActivity(intent)
+        }
+        diceButton.setOnClickListener{
+            var intent= Intent(this,diceButton::class.java)
+            startActivity(intent)
+        }
+
+
+    }
+}
---
+++ app/src/main/java/com/example/guru22/coinButtonresult.kt
@@ -0,0 +1,48 @@
+package com.example.guru22
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.TextView
+
+class coinButtonresult : AppCompatActivity() {
+
+    lateinit var text1 : TextView
+    lateinit var Radio_group : RadioGroup
+    lateinit var button_one : RadioButton
+    lateinit var button_two : RadioButton
+    lateinit var button_three : RadioButton
+    lateinit var button_four : RadioButton
+    lateinit var button_five : RadioButton
+    lateinit var button_six : RadioButton
+    lateinit var button_seven : RadioButton
+    lateinit var btnOK : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coin_buttonresult)
+
+        text1=findViewById(R.id.Text1)
+        Radio_group=findViewById(R.id.radio_group)
+
+        button_one=findViewById(R.id.radio_button_one)
+        button_two=findViewById(R.id.radio_button_two)
+        button_three=findViewById(R.id.radio_three)
+        button_four=findViewById(R.id.radio_button_four)
+        button_five=findViewById(R.id.radio_button_five)
+        button_six=findViewById(R.id.radio_button_six)
+        button_seven=findViewById(R.id.radio_button_seven)
+
+        btnOK=findViewById(R.id.buttonOK)
+
+        btnOK.setOnClickListener{
+            when(Radio_group.checkedRadioButtonId){
+                // 다음 화면이랑 연결
+            }
+        }
+
+
+    }
+}
